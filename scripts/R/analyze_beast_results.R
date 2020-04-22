c3ed3ea4d836838bd7a73825f2b0245e50869541
Maya Lewinsohn <mayalewinsohn@Mayas-MacBook-Pro.local>
2020-04-21 18:55:58 -0700

    add plot sim

--- scripts/R/analyze_beast_results.R
+++ scripts/R/analyze_beast_results.R
@@ -5,39 +5,84 @@ library("Biostrings")
 library("ggplot2")
 library("ggtree")
 library("treeio")
+library("phytools")
+library(cowplot)
+library(biwavelet)
+library(autoimage)
+library(gridGraphics)
+
 
 setwd("/Users/mayalewinsohn/Documents/PhD/Bedford_lab/hcc-tumor-evol")
 beast_file <- system.file("outputs/CC_output.tree", package="ggtree")
 
 CC_sim_tree <- read.beast("outputs/CC_mcc.tree")
-  
-ggtree(CC_sim_tree, aes(color = X_coord))
 
-ggtree(CC_sim_tree, 
-       yscale = "Y_coord")
 
-ggplot(CC_sim_tree, aes(x = X_coord, y = Y_coord)) + geom_point()
+#plot spatial and time phylogenies
 
-str(CC_sim_tree)
-str(as.phylo(CC_sim_tree))
+n <- length(CC_sim_tree@phylo$tip.label)
+m <- CC_sim_tree@phylo$Nnode
+
+#matrix with trait info for leaves
+X <- get.data(CC_sim_tree) %>% 
+  arrange(node) %>% 
+  filter(node %in% 1:n) %>% 
+  select(X_coord, Y_coord) %>% 
+  as.matrix
+
+rownames(X) <- CC_sim_tree@phylo$tip.label
+
+#matrix with trait info for nodes
+A <- get.data(CC_sim_tree) %>% 
+  arrange(node) %>% 
+  filter(node %in% (n+1):(n+m)) %>% 
+  select(X_coord, Y_coord) %>% 
+  as.matrix
+
+rownames(A) <- as.character((n+1):(n+m))
+
+#make continuous color scale 
+
+color.gradient <- function(x, colors=c("red","yellow","springgreen","royalblue"), colsteps=100) {
+  return( colorRampPalette(colors) (colsteps) [ findInterval(x, seq(min(x),max(x), length.out=colsteps)) ] )
+}
+
+#fit color scale to Euclidian distance from center
+col_vec <- c(color.gradient(sqrt(X[,1]^2 + X[,2]^2)), color.gradient(sqrt(A[,1]^2 + A[,2]^2)))
+names(col_vec) <- 1:(n+m)
+
+#make combined figure
+plot.new()
+
+reset.par()
+#dev.new()
+dev.copy(png,"figures/spatial_sim/beast_spatial.png")
+par(xpd = NA, # switch off clipping, necessary to always see axis labels
+    bg = "transparent", # switch off background to avoid obscuring adjacent plots
+    oma = c(2, 2, 0, 0)) # move plot to the right and up
+phylomorphospace(as.phylo(CC_sim_tree),
+                 X,
+                 A=A,
+                 control=list(col.node=col_vec),
+                 xlab = "X_coord",
+                 ylab = "Y_coord",
+                 label='off')
+p1 <- recordPlot()  
+#p3 <- plot_grid(p1)
+#fancyTree(as.phylo(CC_sim_tree),type="scattergram",X=X,A=A,label = "off")
+dev.off()
+p2 <- ggtree(CC_sim_tree) +
+  geom_tippoint(aes(fill=as.factor(node)), pch=21, color = "black", size=2.5) +
+  geom_point2(aes(fill=as.factor(node)),pch=21, color = "black", size=2.5) +
+  scale_fill_manual(values = col_vec)
+
+beast_results <- plot_grid(p2, p1,
+          labels = 'AUTO')
+
+
+save_plot("figures/spatial_sim/beast_phylo.png", p2)
+save_plot("figures/spatial_sim/beast_phylo_spatial.png", beast_results)
+
+
 tree_data <- get.data(CC_sim_tree)
 
-ggtree(CC_sim_tree)
-#+ 
-  geom_tiplab(align=TRUE, linetype='dashed', linesize=.3) + 
-  geom_range("length_0.95_HPD", color='red', size=2, alpha=.5) + 
-  geom_text2(aes(label=round(as.numeric(posterior), 2), 
-                 subset=as.numeric(posterior)> 0.9, 
-                 x=branch), vjust=0) 
-  
-  file <- system.file("extdata/BEAST", "beast_mcc.tree", 
-                      package="treeio")
-  beast <- read.beast(file)
-  ggtree(beast) + 
-    geom_tiplab(align=TRUE, linetype='dashed', linesize=.3) + 
-    geom_range("length_0.95_HPD", color='red', size=2, alpha=.5) + 
-    geom_text2(aes(label=round(as.numeric(posterior), 2), 
-                   subset=as.numeric(posterior)> 0.9, 
-                   x=branch), vjust=0) 
-
-   
--- scripts/R/convert_sim_cells_to_beast_input.R
+++ scripts/R/convert_sim_cells_to_beast_input.R
@@ -16,6 +16,16 @@ CSC_sim_cells <- read.csv("outputs/sim_CSC_cells.csv")
 #temp global var for developing functions
 cells_df <- CC_sim_cells
 
+#first normalize locations so that 0 is the center
+normalize_locs <- function(cells){
+  center_x <- min(cells$locx) + (max(cells$locx) - min(cells$locx))/2
+  center_y <- min(cells$locy) + (max(cells$locy) - min(cells$locy))/2
+  norm_cells <- cells %>% 
+    mutate("norm_locx" = locx - center_x, "norm_locy" = locy - center_y)
+  return(norm_cells)
+}
+
+norm_cells_df <- normalize_locs(cells_df)
 #calculate parameters of simulations
 # calc_sim_stats <- function(cells_df) {
 #   stats_df <- data.frame("avg_mutation_rate" = mean(cells_df$mutation_rate),
@@ -223,5 +233,125 @@ for (i in 2:nrow(final_sampled_cells)) {
 
 }
 
+#find MRCA of sampled cells
+
+collect_all_ancestors <- function(index, all_cells, ancestors = c()) {
+  if( index == 0) {
+    return(ancestors)
+  } else {
+    new_index <- all_cells[all_cells$index == index,"parent_index"]
+    return(collect_all_ancestors(index = new_index, all_cells, ancestors = c(ancestors, new_index)))
+  }
+}
+
+test <- collect_all_ancestors(index = 42, cells_df)
+test_2 <- collect_all_ancestors(index = 153, cells_df)
+test[min(which(test %in% test_2))]
+
+find_MRCA <- function(index_1, index_2, all_cells) {
+  ancestors_1 <- collect_all_ancestors(index_1, all_cells)
+  ancestors_2 <- collect_all_ancestors(index_2, all_cells)
+  mrca <- ancestors_1[min(which(ancestors_1 %in% ancestors_2 ))]
+  return(mrca)
+}
+
+collect_nodes_for_phylo <- function(orphan_cells, all_cells, n, node_vec = c(),
+                                    edge_matrix = matrix(ncol=2), edge_lengths = c()) {
+  all_cells <- all_cells %>% arrange(index)
+  #if just starting record information for leaves
+  if (length(node_vec) == 0) {
+    node_vec <- orphan_cells
+    n <- length(orphan_cells)
+  }
+  if (sum(orphan_cells == 0)/length(orphan_cells) == 1) {
+
+    trait_data <- data.frame("node" = node_vec, "x_coord" = all_cells[node_vec+1,"norm_locx"],
+                             "y_coord" = all_cells[node_vec+1,"norm_locy"])
+  
+    return(list("trait_data" = trait_data, "edge_matrix" = edge_matrix[-1,], "edge_lengths" = edge_lengths))
+  } else {
+    #make all pariwise combinations of cells without mrca
+    all_combs <- as.data.frame(t(combn(orphan_cells, 2)))
+    
+    #find ancestors of candidate cells 
+    common_ancestors <- map2_dbl(all_combs$V1, all_combs$V2, ~find_MRCA(.x, .y, all_cells))
+    common_ancestors_data <- all_cells[common_ancestors+1,]
+    
+    #find youngest ancestor and two children
+    youngest_ancestor <- common_ancestors_data$index[common_ancestors_data$birthdate == max(common_ancestors_data$birthdate)]
+    
+    if (length(youngest_ancestor) == 1) {
+
+    #add new node 
+    node_vec <- c(node_vec, youngest_ancestor)
+
+    child_1 <- all_combs[which(common_ancestors == youngest_ancestor),1]
+    child_2 <- all_combs[which(common_ancestors == youngest_ancestor),2]
+    c1 <- which(node_vec == child_1)
+    c2 <- which(node_vec == child_2)
+    a <- length(node_vec)
+
+    #update edge matrrix
+    edge_matrix <- rbind(edge_matrix, c(a,c1), c(a,c2))
+    
+    if (c1 <= n) {
+      t_c1 <- all_cells[child_1+1, "deathdate"]
+    } else {
+      t_c1 <- all_cells[child_1+1, "birthdate"]
+    }
+    
+    if (c2 <= n) {
+      t_c2 <- all_cells[child_2+1, "deathdate"]
+    } else {
+      t_c2 <- all_cells[child_2+1, "birthdate"]
+    }
+    
+    t_a <- all_cells[youngest_ancestor+1, "birthdate"]
+    
+    edge_lengths <- c(edge_lengths, (t_c1 - t_a), t_c2 - t_a)
+    
+    orphan_cells <- orphan_cells[orphan_cells != child_1]
+    orphan_cells <- orphan_cells[orphan_cells != child_2]
+    orphan_cells <- c(orphan_cells, youngest_ancestor)
+  
+    } else {
+    youngest_ancestor <- youngest_ancestor[1]
+    node_vec <- c(node_vec, youngest_ancestor)
+    children <- unique(unlist(all_combs[which(common_ancestors == youngest_ancestor),]))
+    
+    a <- length(node_vec)
+    t_a <- all_cells[youngest_ancestor+1, "birthdate"]
 
 
+    for (child in children) {
+
+      c <- which(node_vec == child)
+      if (c <= n) {
+        t_c <- all_cells[child_1+1, "deathdate"]
+      } else {
+        t_c <- all_cells[child_1+1, "birthdate"]
+      }
+      edge_matrix <- rbind(edge_matrix, c(a,c))
+      edge_lengths <- c(edge_lengths, t_c - t_a)
+      orphan_cells <- orphan_cells[orphan_cells != child]
+      }
+    orphan_cells <- c(orphan_cells, youngest_ancestor)
+    }
+    return(collect_nodes_for_phylo(orphan_cells, all_cells, n, node_vec = node_vec,
+                                        edge_matrix = edge_matrix, edge_lengths = edge_lengths))
+    
+    
+  }
+  
+}
+leaves <- final_sampled_cells$index
+tree_info <- collect_nodes_for_phylo(leaves, norm_cells_df)
+tr <- list(edge = tree_info$edge_matrix, tip.label = leaves, Nnode = (length(leaves) - 1),
+           edge.length = tree_info$edge_lengths)
+class(tr) <- "phylo"
+
+plot(tr)
+ggtree(tr)
+#for tree
+#nodes list
+find_MRCA(42,153,cells_df)
